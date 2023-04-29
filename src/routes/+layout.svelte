7fe595e7f11afe8e1753ef63a44f7ab3037b4402
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-04-29 12:13:38 +0530

    dksxk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	
 </script>
+<svelte:head>
+       <script src="/bootstrap.bundle.min.js"></script> 
+       <link rel="stylesheet" href="/bootstrap.min.css"> 
+       <link rel="stylesheet" href="/w3.css">
+     </svelte:head>
 
 <div class="app">
 	<h1>hello i m the best</h1>
