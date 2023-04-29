59c29f1611489d87418a7a30a3c339630e95baae
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-04-29 12:14:12 +0530

    abxabj

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	
 </script>
 <svelte:head>
-       <script src="/bootstrap.bundle.min.js"></script> 
        <link rel="stylesheet" href="/bootstrap.min.css"> 
        <link rel="stylesheet" href="/w3.css">
      </svelte:head>
